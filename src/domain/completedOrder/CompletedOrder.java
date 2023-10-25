806c2245d5ac63ba7700fabc1b0c0394af35f8f8
BMDK9 <qnrakdlzm77@naver.com>
2023-10-25 09:16:34 +0900

    내가 이거 때문에 2시간을 하....

---
+++ src/domain/completedOrder/CompletedOrder.java
@@ -0,0 +1,9 @@
+package domain.completedOrder;
+
+import domain.order.Order;
+
+import java.time.Instant;
+
+public class CompletedOrder extends Order {
+    Instant ccompletedOrderDateTime;
+}
---
+++ src/domain/order/Order.java
@@ -0,0 +1,52 @@
+package domain.order;
+
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+public class Order {
+
+    private double totalPrice;
+    private Cart cart; // 이부분 확신이 없습니다.
+    private String requestMessage;
+    private int waitingNumber;
+    private Instant orderDateTime = Instant.parse(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mmZ")
+            .withZone(ZoneOffset.UTC)
+            .format(Instant.now())); //이거 확인 필요합니다.
+
+
+    public String getRequestMessage() {
+        return requestMessage;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public Instant getOrderDateTime() {
+        return orderDateTime;
+    }
+
+    public int getWaitingNumber() {
+        return waitingNumber;
+    }
+
+    public void inputRequestMessage(String message) throws IllegalArgumentException {
+        if (message.length() > 20)
+            throw new IllegalArgumentException("20자 이내로 작성해주세요.");
+        this.requestMessage = message;
+    }
+
+    //    - do 대기번호 발급
+//⇒ issueWaitingNumber() :: int
+//        public int issueWaitingNumber( ??? ) {
+//        this.waitingNumber = ????
+//    };
+    public double getTotalPrice(double cart) {
+        this.cart = cart;
+    } //==========================================이부분 확신이 없습니다.
+}
