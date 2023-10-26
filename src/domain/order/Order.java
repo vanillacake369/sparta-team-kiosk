186271063cb0a38783b783f96db20b1f3d5e4ab2
BMDK9 <qnrakdlzm77@naver.com>
2023-10-26 19:31:26 +0900

    완료주문 목록 서비스 ver.1

--- src/domain/order/Order.java
+++ src/domain/order/Order.java
@@ -10,7 +10,7 @@ public class Order {
     private String requestMessage;          // 3 요청사항
     private Instant orderDateTime;          // 4 주문 일시
     private int waitingNumber = 0;           // 5 대기번호
-
+//====================================================생성자
     public Order(double totalPrice, Cart cart, String requestMessage, Instant orderDateTime, int waitingNumber) {
         this.totalPrice = cart.getTotalPrice();
         this.cart = cart;
@@ -18,7 +18,7 @@ public class Order {
         this.orderDateTime = orderDateTime;
         this.waitingNumber = waitingNumber;
     }
-
+//=================================================메서드 구역
     public Cart getCart() {
         return cart;
     }                                       // 2 장바구니
@@ -35,8 +35,6 @@ public class Order {
         return waitingNumber;
     }                                       //5 대기번호
 
-//==================================================메서드 구역
-
     public void inputRequestMessage(String message) throws IllegalArgumentException {
         if (message.length() > 20) {
             throw new IllegalArgumentException("20자 이내로 작성해주세요.");
@@ -48,8 +46,6 @@ public class Order {
         this.waitingNumber += 1;
         return waitingNumber;
     }                                       // 대기번호 발급
-
-//==================================================생성자 구역
 
 
 }
--- src/service/store/selectCompletedOrders.java
+++ src/service/store/selectCompletedOrders.java
@@ -3,6 +3,19 @@ package service.store;
 
 import domain.completedOrder.CompletedOrder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class selectCompletedOrders {
-    CompletedOrder completedOrder = new CompletedOrder();
+    List<CompletedOrder> completedOrders = new ArrayList<>();
+    public selectCompletedOrders () {
+        completedOrders.forEach(completedOrder -> {
+            System.out.println("주문 번호 : "+completedOrder.getWaitingNumber() +
+                    "주문 상품 : "+ completedOrder.getCart().getProducts() +
+                    "주문 수량 : "+ completedOrder.getCart().getProductCount() +
+                    "주문 금액 : "+ completedOrder.getCart().getTotalPrice() +
+                    "요청 사항 : "+ completedOrder.getRequestMessage());
+        });
+    }
+
 }
