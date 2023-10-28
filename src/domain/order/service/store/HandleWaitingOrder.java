262295b6f996c63331bbcccdb9080789dbad97ce
BMDK9 <qnrakdlzm77@naver.com>
2023-10-29 01:34:22 +0900

    <완료 주문 처리 기능 관련> 15번     CompletedOrder리스트 생성 51번 ~ 56번
    <주문 입력 기능 관련>
    16번     스캐너 입력 따로 분리
    17번     숫자 입력 정규 표현식 생성
    34번     입력 타입을 String으로 변경
    36번~38번     정규식 예외처리
    41번~60번     입력 타입 변경에 따른 코드 변경

--- src/domain/order/service/store/HandleWaitingOrder.java
+++ src/domain/order/service/store/HandleWaitingOrder.java
@@ -7,46 +7,57 @@ import domain.product.entity.Product;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class HandleWaitingOrder {
     List<Order> orders = new ArrayList<>();
+    List<CompletedOrder> completedOrders = new ArrayList<>(); // 51번줄에 사용할 CompletedOrder 리스트
+    private static Scanner sc = new Scanner(System.in); // 34번줄 주문 번호 입력 받기
+    private static final String NUMBER_REG = "^[0-9]*$"; // 36번줄 정규 표현식 => 주문 번호 입력
 
     public HandleWaitingOrder() {
         orders.forEach(bill -> {
             System.out.println("주문 번호 : " + bill.getWaitingNumber() +
-                                "주문 상품 : " + bill.getCart().getProducts()
-                                .stream().map(Product::getName).collect(Collectors.joining(", ")) +
-                                "주문 수량 : " + bill.getCart().getProductCount() +
-                                "주문 금액 : " + bill.getCart().getTotalPrice() +
-                                "요청 사항 : " + bill.getRequestMessage() +
-                                "주문 시간 : " + bill.getOrderDateTime());
+                    "주문 상품 : " + bill.getCart().getProducts()
+                    .stream().map(Product::getName).collect(Collectors.joining(", ")) +
+                    "주문 수량 : " + bill.getCart().getProductCount() +
+                    "주문 금액 : " + bill.getCart().getTotalPrice() +
+                    "요청 사항 : " + bill.getRequestMessage() +
+                    "주문 시간 : " + bill.getOrderDateTime());
         });
 
         while (orders.size() > 0) {  // 대기 주문이 있는 경우
             System.out.println("완료시킬 주문 번호를 입력해주세요." +
-                                "\n뒤로 가고 싶다면 0번을 입력해주세요");
-            Scanner sc = new Scanner(System.in);
-            int handleNumber = sc.nextInt(); // 주문 번호 입력 받는다.
+                    "\n뒤로 가고 싶다면 0번을 입력해주세요");
 
-            if (handleNumber == 0) { // 주문 번호가 0번 이면 while문 종료
+            String handleNumber = sc.nextLine();// 15번 줄을 이용하여 주문 번호 입력 받는다.
+
+            if (!Pattern.matches(NUMBER_REG, handleNumber)) {
+                System.out.println("숫자를 입력해주세요.");
+                continue;
+            }                                           // 입력값이 숫자가 아니면 재시작
+
+            if (handleNumber.equals("0")) { // 주문 번호가 0번 이면 while문 종료
                 break;
-            } else if (handleNumber != 0) {     // 주문 번호 0번을 입력하지 않았을 때
-                System.out.println(orders.get(handleNumber - 1).getWaitingNumber() +  // 입력받은 번호의 주문을 출력한다
-                        "번 주문을 완료하시겠습니까?" +
+            } else if (!handleNumber.equals("0")) {     // 주문 번호 0번을 입력하지 않았을 때
+                System.out.println(orders.get(Integer.parseInt(handleNumber) - 1).getWaitingNumber() +
+                        "번 주문을 완료하시겠습니까?" +  // 입력받은 번호의 주문을 출력한다
                         "\n1. 예     2. 아니오");
-                int doneItNumber = sc.nextInt(); // 주문의 완료 여부를 결정한다.
-                switch (doneItNumber) {
+
+                String doneItNumber = sc.nextLine(); // 주문의 완료 여부를 결정한다.
+                switch (Integer.parseInt(doneItNumber)) {
                     case 1:
-                        // orders.get(handleNumber - 1); 를 완료 주문으로 보낸다. 어디로 보내야하지??
-                        orders.remove(handleNumber-1); // 완료 처리한 주문은 대기 주문 목록에서 삭제한다.
+                        completedOrders.add((CompletedOrder) orders.get(Integer.parseInt(handleNumber) - 1));
+                        // CompletedOrder에 주문을 추가한다.
+                        orders.remove(Integer.parseInt(handleNumber) - 1);
+                        // 완료 처리한 주문은 대기 주문 목록에서 삭제한다.
                         System.out.println("주문이 완료되었습니다.");
                         break;
+
                     case 2:
                         System.out.println("주문이 완료되지 않았습니다.");
                         break;
-                    default:
-                        System.out.println("올바른 번호를 입력해주세요.");
                 }
             }
         }
