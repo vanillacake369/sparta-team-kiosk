5033450d6070c9f96e56ee34a5cf9717f6838ac9
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-10-29 00:16:27 +0900

    feat : 장바구니 관련 뷰 구현

--- src/main/domain/menu/client/ClientMenuView.java
+++ src/main/domain/menu/client/ClientMenuView.java
@@ -2,7 +2,9 @@ package main.domain.menu.client;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import main.domain.cart.Cart;
 import main.domain.menu.entity.Menu;
+import main.domain.product.entity.Product;
 import main.kiosk.MenuBoard;
 
 public class ClientMenuView {
@@ -21,5 +23,27 @@ public class ClientMenuView {
 
     public static void printProductsOf(Menu menu) {
         System.out.println(menu.printAllProducts());
+    }
+
+    public static void askAddCart(Product product, Cart cart) {
+        // 담으려는 아이템
+        System.out.println("========       Product     ========");
+        System.out.println(product.toString());
+        System.out.println("===================================");
+        // 장바구니 상태
+        System.out.println("========       Cart     ========");
+        System.out.println(cart.toString());
+        System.out.println("===================================");
+
+        // 출력여부
+        System.out.println("해당 상품을 장바구니에 담으시겠습니까?");
+    }
+
+    public static void doneAddCart() {
+        System.out.println("장바구니 담기가 완료되었습니다.");
+    }
+
+    public static void abortAddCart() {
+        System.out.println("장바구니 담기가 취소되었습니다.");
     }
 }
