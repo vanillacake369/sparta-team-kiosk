cb704043cf6e835b3fd923aa9d7e247f5f3f9160
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-10-28 22:07:10 +0900

    feat : 상점용 메인 메뉴 서비스

--- src/main/kiosk/StoreKioskController.java
+++ src/main/kiosk/StoreKioskController.java
@@ -1,6 +1,10 @@
 package main.kiosk;
 
+import main.domain.menu.store.StoreMenuView;
+
 public class StoreKioskController {
     public void runProgram() {
+        StoreMenuView.printAllKioskOptions();
+        
     }
 }
