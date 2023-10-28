53532221def57ba5ac703e525c65074b9ed9cd7e
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-10-28 20:39:58 +0900

    feat : 메뉴판에 대한 싱글톤인 MenuBoard 구현

--- src/main/kiosk/ClientKioskController.java
+++ src/main/kiosk/ClientKioskController.java
@@ -2,7 +2,7 @@ package main.kiosk;
 
 public class ClientKioskController {
     // 메뉴판 인스턴스
-    private static final MenuBoardState menuBoardState = MenuBoardState.getInstance();
+    private static final MenuBoard menuBoard = MenuBoard.getInstance();
 
     public void runProgram() {
     }
---
+++ src/main/kiosk/MenuBoard.java
@@ -0,0 +1,49 @@
+package main.kiosk;
+
+import java.util.ArrayList;
+import java.util.List;
+import main.domain.menu.entity.Menu;
+import main.domain.product.entity.Product;
+
+public class MenuBoard {
+    private static MenuBoard menuBOard;
+    private final List<Menu> menus = new ArrayList<>();
+
+    private MenuBoard() {
+    }
+
+    public static MenuBoard getInstance() {
+        if (menuBOard == null) {
+            menuBOard = new MenuBoard();
+        }
+        menuBOard.initMenuBoard();
+        return menuBOard;
+    }
+
+    // 메뉴 상태 초기화
+    final void initMenuBoard() {
+        // 메뉴 생성
+        Menu burger = createBurgerMenu();
+
+        //메뉴들 <- 메뉴
+        menus.add(burger);
+    }
+
+    Menu createBurgerMenu() {
+
+        // 제품 리스트
+        List<Product> burgers = new ArrayList<>();
+
+        // 제품 생성부
+        Product burger = Product.createProduct("불고기버거", 6.5, "불고기로 만든 햄버거");
+        burgers.add(burger);
+
+        // 메뉴 반환
+        Menu burgerMenu = Menu.createMenu("버거메뉴", "햄버거에 대한 메뉴입니다.", burgers);
+        return burgerMenu;
+    }
+
+    public List<Menu> getMenus() {
+        return menus;
+    }
+}
