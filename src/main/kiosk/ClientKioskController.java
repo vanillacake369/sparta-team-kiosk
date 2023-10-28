b6aa61b45955770461c4f3ebae0a77b7352c9056
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-10-29 00:17:52 +0900

    feat : ClientKioskController > runProgram 내 로직 => 함수 추출

--- src/main/kiosk/ClientKioskController.java
+++ src/main/kiosk/ClientKioskController.java
@@ -1,27 +1,60 @@
 package main.kiosk;
 
+import main.domain.cart.Cart;
 import main.domain.menu.client.ClientMenuService;
 import main.domain.menu.client.ClientMenuView;
 import main.domain.menu.entity.Menu;
+import main.domain.product.entity.Product;
+import main.utils.ConsoleInput;
 
 public class ClientKioskController {
     // 메뉴판 인스턴스
     private static final MenuBoard menuBoard = MenuBoard.getInstance();
 
     private static final ClientMenuService clientMenuService = new ClientMenuService();
+    private static final Cart cart = Cart.getInstance();
 
     public void runProgram() throws Exception {
         // 메뉴
         ClientMenuView.printAllMenus(menuBoard);
 
         // 메뉴 선택
-        int id = 1;
+        Menu menu = selectMenu();
 
-        // 해당 메뉴 찾기
-        Menu menu = clientMenuService.findById(menuBoard, id);
-        // 해당 메뉴의 상품들 출력
+        // 해당 메뉴에 대한 상품 선택
+        Product product = selectProduct(menu);
+
+        // 장바구니에 담기
+        addCart(cart, product);
+    }
+
+    private Menu selectMenu() {
+        int menuId = getInput();
+        Menu menu = clientMenuService.findMenuById(menuBoard, menuId);
         ClientMenuView.printProductsOf(menu);
+        return menu;
+    }
 
+    private Product selectProduct(Menu menu) {
+        int productId = getInput();
+        Product product = clientMenuService.findProductById(menu, productId);
+        ClientMenuView.askAddCart(product, cart);
+        return product;
+    }
 
+    private void addCart(Cart cart, Product product) {
+        int addCartOption = getInput();
+        if (addCartOption == 1) {
+            cart.addProduct(product);
+            ClientMenuView.doneAddCart();
+        } else {
+            ClientMenuView.abortAddCart();
+        }
+    }
+
+    private int getInput() throws IllegalArgumentException {
+        ClientDto clientDto = new ClientDto(ConsoleInput.readNextLine());
+        ClientDtoValidator.validateDto(clientDto);
+        return ClientDtoMapper.mapClientDto(clientDto);
     }
 }
