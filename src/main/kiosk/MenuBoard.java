6374b9257bedbb8f7d5b22adf6d522e4a44beaa6
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-10-28 22:43:49 +0900

    update : 각 메뉴 생성 로직 수정

--- src/main/kiosk/MenuBoard.java
+++ src/main/kiosk/MenuBoard.java
@@ -22,28 +22,39 @@ public class MenuBoard {
 
     // 메뉴 상태 초기화
     final void initMenuBoard() {
-        // 메뉴 생성
-        Menu burger = createBurgerMenu();
-
         //메뉴들 <- 메뉴
-        menus.add(burger);
+        menus.add(createBurgerMenu());
+        menus.add(createDrinkMenu());
     }
 
-    Menu createBurgerMenu() {
+    private Menu createBurgerMenu() {
         // 제품 리스트
-        List<Product> burgers = new ArrayList<>();
+        List<Product> products = new ArrayList<>();
 
         // 제품 생성부
         Product bulgogi = Product.createProduct("불고기버거", 6.5, "불고기로 만든 햄버거");
         Product cheese = Product.createProduct("치즈버거", 6.5, "불고기로 만든 햄버거");
         Product mac = Product.createProduct("맥", 6.5, "불고기로 만든 햄버거");
-        burgers.add(bulgogi);
-        burgers.add(cheese);
-        burgers.add(mac);
+        products.add(bulgogi);
+        products.add(cheese);
+        products.add(mac);
 
         // 메뉴 반환
-        Menu burgerMenu = Menu.createMenu("버거메뉴", "햄버거에 대한 메뉴입니다.", burgers);
-        return burgerMenu;
+        return Menu.createMenu("버거메뉴", "햄버거에 대한 메뉴입니다.", products);
+    }
+
+    private Menu createDrinkMenu() {
+        // 제품 리스트
+        List<Product> products = new ArrayList<>();
+
+        // 제품 생성부
+        Product tomatoJuice = Product.createProduct("토마토주스", 4.0, "토마토로 만든 주스");
+        Product orangeJuice = Product.createProduct("오렌지주스", 4.0, "오렌지로 만든 주스");
+        products.add(tomatoJuice);
+        products.add(orangeJuice);
+
+        // 메뉴 반환
+        return Menu.createMenu("음료메뉴", "음료에 대한 메뉴입니다.", products);
     }
 
     public List<Menu> getMenus() {
