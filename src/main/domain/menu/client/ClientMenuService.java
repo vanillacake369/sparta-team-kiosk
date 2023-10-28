e94747a696881a06a957684b8bcd0549e5af0388
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-10-29 00:16:49 +0900

    feat : 메뉴에서 id를 통한 상품찾기 구현

--- src/main/domain/menu/client/ClientMenuService.java
+++ src/main/domain/menu/client/ClientMenuService.java
@@ -1,14 +1,22 @@
 package main.domain.menu.client;
 
 import main.domain.menu.entity.Menu;
+import main.domain.product.entity.Product;
 import main.kiosk.MenuBoard;
 
 public class ClientMenuService {
 
-    public Menu findById(MenuBoard menuBoard, int id) throws Exception {
+    public Menu findMenuById(MenuBoard menuBoard, int menuId) throws IllegalArgumentException {
         return menuBoard.getMenus().stream()
-                .filter(menu -> menu.getId() == id)
+                .filter(menu -> menu.getId() == menuId)
                 .findFirst()
-                .orElseThrow(IllegalArgumentException::new);
+                .orElseThrow(() -> new IllegalArgumentException("찾고자 하시는 메뉴가 없습니다."));
+    }
+
+    public Product findProductById(Menu menu, int productId) throws IllegalArgumentException {
+        return menu.getProducts().stream()
+                .filter(product -> product.getId() == productId)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("찾고자 하시는 상품이 없습니다."));
     }
 }
