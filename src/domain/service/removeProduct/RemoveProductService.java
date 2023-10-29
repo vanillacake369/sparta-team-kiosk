9e0a191854d686b8d1db258f2b361dc2ba726078
RyanDJang <jangd6995@gmail.com>
2023-10-29 20:52:38 +0900

    상품 생성과 삭제 초안. 따로 만들면 작동하나 합치니 에러 뜸

---
+++ src/domain/service/removeProduct/RemoveProductService.java
@@ -0,0 +1,21 @@
+package service;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class RemoveProductService {
+    public static void removeProduct(Scanner scanner, List<StoreProducts> products) {
+        System.out.print("삭제할 상품 ID: ");
+        int productId = scanner.nextInt();
+
+        for (StoreProducts product : products) {
+            if (product.getId() == productId) {
+                products.remove(product);
+                System.out.println("상품 삭제 완료.");
+                return;
+            }
+        }
+
+        System.out.println("상품을 찾을 수 없습니다.");
+    }
+}
---
+++ src/domain/service/storeProducts/StoreProducts.java
@@ -0,0 +1,25 @@
+package service;
+
+public class StoreProducts {
+    private int id;
+    private String name;
+    private double price;
+
+    public StoreProducts(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
